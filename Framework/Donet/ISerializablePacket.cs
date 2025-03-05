fa5ce9cc294e9d2f3822693a91ad46b01f563362
heedohee <ydohee0907@gmail.com>
2025-03-05 19:22:38 +0900

    reset

--- Framework/Donet/ISerializablePacket.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Donet
-{
-    public interface ISerializablePacket
-    {
-        public void OnReceived(ISession session);
-        public void OnSerialize(Serializer serializer); // use serializer for easy serialize
-        public ISerializablePacket CreatePacket();
-    }
-}
